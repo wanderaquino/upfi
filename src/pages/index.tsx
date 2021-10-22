721c11c475636a633fd8c965f5034ffaef1e9206
Wander Aquino <wander.aquino@gmail.com>
2021-10-22 01:17:04 -0300

    Add load more button if hasNextPage is true

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { Button, Box } from '@chakra-ui/react';
+import { Button, Box, Flex } from '@chakra-ui/react';
 import { useMemo } from 'react';
 import { useInfiniteQuery } from 'react-query';
 
@@ -22,7 +22,12 @@ export default function Home(): JSX.Element {
     isFetchingNextPage,
     fetchNextPage,
     hasNextPage,
-  } = useInfiniteQuery('images', fetchImages);
+  } = useInfiniteQuery('images', fetchImages, {
+    getNextPageParam: (lastPage, pages) => {
+      return  pages[0].after;
+      }
+    }
+  );
 
   const formattedData = useMemo(() => {
     // TODO FORMAT AND FLAT DATA ARRAY
@@ -44,6 +49,12 @@ export default function Home(): JSX.Element {
         ) : (
           <Box maxW={1120} px={20} mx="auto" my={20}>
             <CardList cards={formattedData} />
+            {
+              hasNextPage &&
+                <Flex mt="1rem">
+                  <Button>Carregar mais...</Button>
+                </Flex> 
+            }
             {/* TODO RENDER LOAD MORE BUTTON IF DATA HAS NEXT PAGE */}
         </Box>
         ) 
