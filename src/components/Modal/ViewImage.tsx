13f87934210755783f85d82861cbeadd239ee05a
Wander Aquino <wander.aquino@gmail.com>
2021-10-26 23:25:12 -0300

    Use viewImage modal to display image

--- src/components/Modal/ViewImage.tsx
+++ src/components/Modal/ViewImage.tsx
@@ -19,5 +19,25 @@ export function ModalViewImage({
   onClose,
   imgUrl,
 }: ModalViewImageProps): JSX.Element {
-  // TODO MODAL WITH IMAGE AND EXTERNAL LINK
+  return (
+    <>
+      <Modal onClose={onClose} isOpen={isOpen}>
+        <ModalOverlay>
+          <ModalContent mx="auto" my="auto" maxW="870px" >
+            <ModalBody m="0" p="0" background="pGray.800">
+              <Image m="0" objectFit="contain" src={imgUrl} />
+            </ModalBody>
+            <ModalFooter 
+              borderBottomLeftRadius="5px" 
+              borderBottomEndRadius="5px"  
+              display="flex" 
+              justifyContent="flex-start" 
+              background="pGray.800" p=".3rem">
+                <Link href={imgUrl} color="pGray.50" fontSize=".8rem" isExternal>Abrir Original</Link>
+            </ModalFooter>
+          </ModalContent>
+        </ModalOverlay>
+      </Modal>
+    </>
+  )
 }
