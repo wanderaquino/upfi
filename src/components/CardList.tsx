702746127037aa954c02376be625d7881049e0e3
Wander Aquino <wander.aquino@gmail.com>
2021-10-26 23:27:25 -0300

    Display modal on image click

--- src/components/CardList.tsx
+++ src/components/CardList.tsx
@@ -17,19 +17,29 @@ interface CardsProps {
 
 export function CardList({ cards }: CardsProps): JSX.Element {
   // TODO MODAL USEDISCLOSURE
+  const {isOpen, onOpen, onClose} = useDisclosure();
 
   // TODO SELECTED IMAGE URL STATE
 
-  // TODO FUNCTION HANDLE VIEW IMAGE
+  const [imgUrl, setImgUrl] = useState("")
+
+  function handleViewImage(url: string) {
+    setImgUrl(url);
+    onOpen();
+  }
 
   return (
     <>
       <SimpleGrid columns={3} spacing={10}>
       {cards.map(card => {
           return (
-            <Card key={card.id} data={card} viewImage={()=>{}}></Card>
+            <Card key={card.id} data={card} viewImage={handleViewImage}></Card>
           )
         })}
+      {
+        isOpen && 
+        <ModalViewImage imgUrl={imgUrl}  onClose={onClose} isOpen={isOpen}/>
+      }
       </SimpleGrid>
     </>
   );
