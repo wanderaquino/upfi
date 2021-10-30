39a1ae6480e2fdfe22a71ac56aafb327dcb308d0
Wander Aquino <wander.aquino@gmail.com>
2021-10-30 20:03:33 -0300

    Switch component fileInput name to image

--- src/components/Form/FormAddImage.tsx
+++ src/components/Form/FormAddImage.tsx
@@ -74,9 +74,11 @@ export function FormAddImage({ closeModal }: FormAddImageProps): JSX.Element {
   };
 
   return (
+    
     <Box as="form" width="100%" onSubmit={handleSubmit(onSubmit)}>
       <Stack spacing={4}>
         <FileInput
+          name="image"
           setImageUrl={setImageUrl}
           localImageUrl={localImageUrl}
           setLocalImageUrl={setLocalImageUrl}
